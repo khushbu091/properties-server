93afbc1a8ed405257105c6f92bfa13862f0834a2
abhik199 <abhishekkirar2004@gmail.com>
2025-03-13 18:36:09 +0530

    two

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 4005;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(express.static(path.join(__dirname, "./public")));
 // app.use(express.static(path.join(__dirname, "./public/build")));
